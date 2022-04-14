aa7462c043aa96659b7d2ade6834f0c29f53be00
usualtool <52517593+usualtool@users.noreply.github.com>
2022-04-14 16:20:07 +0800

    Update Start.php

--- src/Start.php
+++ src/Start.php
@@ -1,1 +1,18 @@
-
+<?php
+namespace usualtool\Workerman;
+use Workerman\Worker;
+class Start{
+    public function __construct(){
+        if(strpos(strtolower(PHP_OS), 'win') === 0){
+            echo "start.php not support windows, please use start_for_win.bat\n";
+        }
+        if(!extension_loaded('pcntl')){
+            echo "Please install pcntl extension.\r\n";
+        }
+        if(!extension_loaded('posix')){
+            echo "Please install posix extension.\r\n";
+        }
+        define('GLOBAL_START', 1);
+        Worker::runAll();
+    }
+}
